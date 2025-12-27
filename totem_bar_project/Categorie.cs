61be2e031471a08b87def1a24d2afc2328cda447
Leonardo16-bit <leo.ramella08@gmail.com>
2025-12-27 11:17:44 +0100

    Classe
    Stesura della classe per le categorie di cibi, bevande, ecc...

---
+++ totem_bar_project/Categorie.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totem_bar_project
+{
+
+    public enum TipoCategoria { Bevande, Panini, Snack, BevandeCalde };
+    internal class Categorie
+    {
+        string nome;
+        int prezzo;
+        private TipoCategoria categoria;
+
+        public Categorie(string nome, int prezzo, TipoCategoria categoria)
+        {
+            this.nome = nome;
+            this.prezzo = prezzo;
+            this.categoria = categoria;
+        }
+        
+        public string GetNome()
+        {
+            return nome;
+        }
+        public int GetPrezzo()
+        {
+            return prezzo;
+        }
+
+        public void SetCategoria(TipoCategoria categoria)
+        { 
+            this.categoria = categoria; 
+        }
+
+        public TipoCategoria GetCategoria()
+        { 
+            return categoria; 
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void SetPrezzo(int prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+    }
+}
