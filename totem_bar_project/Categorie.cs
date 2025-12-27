4d75b90580241a942fa8fef82bb068f241f1ce9f
Leonardo16-bit <leo.ramella08@gmail.com>
2025-12-27 11:36:42 +0100

    Classe
    Miglioramento della classe Categorie

--- totem_bar_project/Categorie.cs
+++ totem_bar_project/Categorie.cs
@@ -10,31 +10,61 @@ namespace totem_bar_project
     public enum TipoCategoria { Bevande, Panini, Snack, BevandeCalde };
     internal class Categorie
     {
-        string nome;
-        int prezzo;
+        private string nome;
+        private int prezzo;
         private TipoCategoria categoria;
+        private string descrizione;
+        private int Iva;
+        private List<string> allergeni = new List<string>();
 
-        public Categorie(string nome, int prezzo, TipoCategoria categoria)
+        public Categorie(string nome, int prezzo, TipoCategoria categoria, string descrizione, int Iva, List<string> allergeni)
         {
             this.nome = nome;
             this.prezzo = prezzo;
             this.categoria = categoria;
+            this.descrizione = descrizione;
+            this.Iva = Iva;
+            this.allergeni = allergeni;
         }
-        
+
+
+        public List<string> GetAllergeni()
+        { 
+            return allergeni; 
+        }
+        public void AggiungiAllergene(string allergene)
+        { 
+            allergeni.Add(allergene); 
+        }
         public string GetNome()
         {
             return nome;
+        }
+        public int GetIva()
+        {
+            return Iva;
+        }
+        public string GetDescrizione()
+        {
+            return descrizione;
+        }
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+
+        public void SetIva(int iva)
+        {
+            Iva = iva;
         }
         public int GetPrezzo()
         {
             return prezzo;
         }
-
         public void SetCategoria(TipoCategoria categoria)
         { 
             this.categoria = categoria; 
         }
-
         public TipoCategoria GetCategoria()
         { 
             return categoria; 
@@ -43,9 +73,14 @@ namespace totem_bar_project
         {
             this.nome = nome;
         }
+
         public void SetPrezzo(int prezzo)
         {
             this.prezzo = prezzo;
+        }
+        public int CalcolaPrezzoConIva()
+        {
+            return prezzo + (prezzo * Iva / 100);
         }
     }
 }
